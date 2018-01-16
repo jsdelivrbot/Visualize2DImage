b2814e2871ce6371beb9863ee2acb60a28d85b16
YonePC <YonePC@YONE>
2018-01-16 05:51:01 +0000

    refactor: setData

--- lessons/04/demo.js
+++ lessons/04/demo.js
@@ -158,38 +158,43 @@ function init() {
     animate();
 }
 
-// init threeJS...
 init();
 
-var data = [
-    '000183.dcm',
-    '000219.dcm',
-    '000117.dcm',
-    '000240.dcm',
-    '000033.dcm',
-    '000060.dcm',
-    '000211.dcm',
-    '000081.dcm',
-    '000054.dcm',
-    '000090.dcm',
-    '000042.dcm',
-    '000029.dcm',
-    '000239.dcm',
-    '000226.dcm',
-    '000008.dcm',
-    '000128.dcm',
-    '000089.dcm',
-    '000254.dcm',
-    '000208.dcm',
-    '000047.dcm',
-    '000067.dcm'
-];
+var {rawgit, dataFullPath} = setData();
 
-var rawgit = 'https://cdn.rawgit.com/FNNDSC/data/master/dicom/andrei_abdomen/';
+function setData() {
+    var data = [
+        '000183.dcm',
+        '000219.dcm',
+        '000117.dcm',
+        '000240.dcm',
+        '000033.dcm',
+        '000060.dcm',
+        '000211.dcm',
+        '000081.dcm',
+        '000054.dcm',
+        '000090.dcm',
+        '000042.dcm',
+        '000029.dcm',
+        '000239.dcm',
+        '000226.dcm',
+        '000008.dcm',
+        '000128.dcm',
+        '000089.dcm',
+        '000254.dcm',
+        '000208.dcm',
+        '000047.dcm',
+        '000067.dcm'
+    ];
 
-var dataFullPath = data.map(function (v) {
-    return rawgit + 'data/' + v;
-});
+    var rawgit = 'https://cdn.rawgit.com/FNNDSC/data/master/dicom/andrei_abdomen/';
+
+    var dataFullPath = data.map(function (currentData) {
+        return rawgit + 'data/' + currentData;
+    });
+    return {rawgit, dataFullPath};
+}
+
 
 var labelmap = ['000000.dcm'];
 
